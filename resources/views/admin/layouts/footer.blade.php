6616046f313290ed27edf341e619cdf7b28b933c
viettd <viettd@hblab@hblab.vn>
2022-04-12 08:08:57 +0700

    fix: slide bar

--- resources/views/admin/layouts/footer.blade.php
+++ resources/views/admin/layouts/footer.blade.php
@@ -1,5 +1,5 @@
-<script src="{{asset('js/admin/sb-admin-2.min.js')}}"></script>
-<script src="{{asset('js/admin/jquery-easing/jquery.easing.min.js')}}"></script>
-<script src="{{asset('js/admin/jquery/jquery.min.js')}}"></script>
-<script src="{{asset('js/admin/bootstrap/bootstrap.bundle.min.js')}}"></script>
-<script src="{{ asset('js/app.js') }}" defer></script>
+<script type="text/javascript" src="{{asset('js/admin/jquery/jquery.min.js')}}"></script>
+<script type="text/javascript" src="{{asset('js/admin/jquery-easing/jquery.easing.min.js')}}"></script>
+<script type="text/javascript" src="{{asset('js/admin/bootstrap/bootstrap.bundle.min.js')}}"></script>
+<script type="text/javascript" src="{{asset('js/admin/sb-admin-2.min.js')}}"></script>
+<script type="text/javascript" src="{{ asset('js/app.js') }}" defer></script>
