7d0bbd66eaa0d862195b1cd95cf93c5e2dd33340
tainv <tainv@hblab.vn>
2022-04-27 00:41:47 +0700

    change thumnail

--- database/seeders/NewsSeeder.php
+++ database/seeders/NewsSeeder.php
@@ -30,7 +30,7 @@ class NewsSeeder extends Seeder
             DB::table('news')->insert([
                 'title' => $faker->text(100),
                 'content' => $faker->text,
-                'thumbnail' => 'https://scontent.fhan5-11.fna.fbcdn.net/v/t39.30808-6/279212253_5116436638403439_4997386856998315697_n.jpg?_nc_cat=103&ccb=1-5&_nc_sid=730e14&_nc_ohc=CBYIlze5ACgAX9Vh60n&_nc_ht=scontent.fhan5-11.fna&oh=00_AT9SIG_gGEblV0i-dkiqJmx0rMAAiLCqN_0LjJLMWB1UOA&oe=626CFEF1',
+                'thumbnail' => 'https://cdvht.s3.amazonaws.com/news/portfolio-5.jpg',
                 'verify' => $faker->randomElement($listVerify),
                 'hot' => $faker->randomElement($listHot),
                 'description' => $faker->paragraph($nbSentences = 3, $variableNbSentences = true),
