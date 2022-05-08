b22715d441aac0655e04f3dca387d90a27c8c663
tainv <tainv@hblab.vn>
2022-05-09 01:12:56 +0700

    fix: send email

--- config/app.php
+++ config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'https://cdvht.herokuapp.com'),
+    'url' => env('APP_URL', 'https://congdoanvinhhatinh.com'),
 
     'asset_url' => env('ASSET_URL', null),
 
